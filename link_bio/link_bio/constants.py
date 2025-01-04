52e5514e9a32f602bde9507517dc6ebb0278ca01
leo <145478507+leocovess@users.noreply.github.com>
2025-01-04 16:48:11 +0100

    Constants created

---
+++ link_bio/link_bio/constants.py
@@ -0,0 +1,12 @@
+#URL
+
+class URL:
+    # URLS
+    URL_LINKEDIN = "https://www.linkedin.com/in/leo-coves-guzman-b68554281/"
+    URL_GITHUB = "https://github.com/LeoCoves"
+    URL_TWITTER = "https://twitter.com/LeoCoves"
+    URL_INSTAGRAM = "https://www.instagram.com/leocovess/"
+    URL_GMAIL = "https://mail.google.com/mail/?view=cm&fs=1&to=l.covesguzman@gmail.com"
+    URL_CV = "/download/CVdev.pdf"
+    URL_WHATSAPP = "https://web.whatsapp.com/send?phone=34675181093"
+    URL_REFLEX = "https://reflex.dev/docs/library/"
